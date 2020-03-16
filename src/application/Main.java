ef42cd3af9952d1edfaff0f438b3f88023dfd96f
Andrew5mith <agsmithis@gmail.com>
2020-03-16 07:17:19 -0500

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,21 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -16,24 +23,55 @@ import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
+	
+	private Stage window;
+	private Scene titleScene, settingsScene;
+	private ComboBox<String> backgroundDropDown = new ComboBox<>();
+	private TextField displayNameField = new TextField("User");
+	private Button saveBtn = new Button("Save");
+	private Button cancelBtn = new Button("Cancel");
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage) throws Exception {
 		try {
-			BorderPane pane = new BorderPane();
-			pane.setCenter(getImage());
-			pane.setBottom(getButtons());
-			Scene scene = new Scene(pane,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.getIcons().add(new Image("WarGamesIcon.png"));
-			primaryStage.setTitle("War Games");
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			window = primaryStage;
+			
+			BorderPane titlePane = new BorderPane();
+			titlePane.setCenter(getImage());
+			titlePane.setBottom(getTitleButtons());
+			titleScene = new Scene(titlePane,400,400);
+			titleScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			BorderPane settingsPane = new BorderPane();
+			settingsPane.setCenter(getSettings());
+			settingsPane.setBottom(getSettingsButtons());
+			settingsScene = new Scene(settingsPane, 400, 400);
+			settingsScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			window.getIcons().add(new Image("WarGamesIcon.png"));
+			window.setTitle("War Games");
+			window.setScene(titleScene);
+			window.show();
+		
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	private HBox getButtons() {
+	private GridPane getSettings() {	
+		GridPane settingsPane = new GridPane();
+		settingsPane.setHgap(5);
+		settingsPane.setVgap(5);
+		settingsPane.add(new Label("Display Name:"), 0, 0);
+		settingsPane.add(displayNameField, 1, 0);
+		settingsPane.add(new Label("Background:"), 0, 1);
+		settingsPane.add(backgroundDropDown, 1, 1);
+		settingsPane.setAlignment(Pos.CENTER);
+		return settingsPane;
+		
+	}
+	
+	private HBox getTitleButtons() {
 		HBox hBox = new HBox(15);
 		hBox.setPadding(new Insets(15,15,15,15));
 		hBox.setAlignment(Pos.CENTER);
@@ -41,9 +79,24 @@ public class Main extends Application {
 		Button settingsBtn = new Button("Settings");
 		playBtn.setId("round-red");
 		settingsBtn.setId("round-red");
+		settingsBtn.setOnAction(e -> window.setScene(settingsScene));
 		hBox.getChildren().addAll(playBtn, settingsBtn);
 		return hBox;
 	}
+	
+	private HBox getSettingsButtons() {
+		HBox hBox = new HBox(15);
+		hBox.setPadding(new Insets(15,15,15,15));
+		hBox.setAlignment(Pos.CENTER);
+		Button cancelBtn = new Button("Cancel");
+		Button saveBtn = new Button("Save");
+		cancelBtn.setId("round-red");
+		saveBtn.setId("round-red");
+		cancelBtn.setOnAction(e -> window.setScene(titleScene));
+		hBox.getChildren().addAll(cancelBtn, saveBtn);
+		return hBox;
+	}
+	
 	
 	private HBox getImage() {
 		HBox hBox = new HBox(25);
